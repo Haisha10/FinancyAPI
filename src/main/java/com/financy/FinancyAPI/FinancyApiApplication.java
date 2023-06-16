1870479ee26fbaddb191782b3d9eb8a37368a27d
AlvaroL <u202012770@upc.edu.pe>
2023-06-16 16:41:52 -0500

    test: railway

--- src/main/java/com/financy/FinancyAPI/FinancyApiApplication.java
+++ src/main/java/com/financy/FinancyAPI/FinancyApiApplication.java
@@ -3,6 +3,10 @@ package com.financy.FinancyAPI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = {@Server(url = "/")})
 @SpringBootApplication
 public class FinancyApiApplication {
 
